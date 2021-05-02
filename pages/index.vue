5128769c9af119737e4690ebbddbf123012f86fe
Roman Gamanetc <r.gamanets@handy-soft.ru>
2021-05-02 22:16:48 +0300

    Чистый проект

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <div class="text-center">
+        <h1>Главная страница</h1>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+
+export default {
+  }
+</script>
